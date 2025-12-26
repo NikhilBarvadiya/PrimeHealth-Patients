d578a45160f885646f8a84a692be64c877036114
Nikhil Barvadiya <nikhilkumarbarvadiya@example.com>
2025-12-26 18:11:20 +0530

    login code fixed...!

--- lib/views/auth/login/login_ctrl.dart
+++ lib/views/auth/login/login_ctrl.dart
@@ -23,8 +23,9 @@ class LoginCtrl extends GetxController {
       String getToken = await CallingService().getToken() ?? "";
       final loginRequest = {'mobileNo': mobileCtrl.text.trim(), "fcm": getToken};
       final loginResponse = await authService.login(loginRequest);
-      if (loginResponse != null && loginResponse['patient']?["id"] != null) {
-        Get.toNamed(AppRouteNames.verifyOtp, arguments: {'mobileNo': mobileCtrl.text.trim(), 'patientId': loginResponse['patient']["id"]});
+      if (loginResponse != null) {
+        final patientId = loginResponse['patient']["id"] ?? loginResponse['patient']["_id"];
+        Get.toNamed(AppRouteNames.verifyOtp, arguments: {'mobileNo': mobileCtrl.text.trim(), 'patientId': patientId});
       }
     } catch (err) {
       toaster.error('Login error: ${err.toString()}');
